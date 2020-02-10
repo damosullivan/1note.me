1a0875f4821576a7a9f995af25c9828d33c1d7ab
Damien O'Sullivan <damosullivan@gmail.com>
2020-02-10 16:10:50 +1100

    Using a 302 redirect, seems correct

--- functions/open/open.js
+++ functions/open/open.js
@@ -3,7 +3,7 @@ exports.handler = async (event, context) => {
   try {
     const key = event.queryStringParameters.key || 'test'
     return {
-      statusCode: 303,
+      statusCode: 302, // Found - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302
       headers: {
         Location: 'https://google.com/search?q=' + key
       }

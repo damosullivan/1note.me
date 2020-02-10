f6430888a7162eeb70108fa0b53a4d0855b66364
Damien O'Sullivan <damosullivan@gmail.com>
2020-02-10 16:01:02 +1100

    Added simple redirect function. Still need AWS integration

--- functions/open/open.js
+++ functions/open/open.js
@@ -1,13 +1,12 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const key = event.queryStringParameters.key || 'test'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      headers: {
+        Location: 'https://google.com/search?q=' + key
+      }
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }

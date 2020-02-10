39eeca90f7b0c030710eb69d66b811699cf4ae99
Damien O'Sullivan <damosullivan@gmail.com>
2020-02-10 16:44:24 +1100

    Setting up the NEW function to accept post and links

--- functions/new/new.js
+++ functions/new/new.js
@@ -1,13 +1,17 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    if (event.httpMethod !== "POST") {
+      // error
+    }
+    const body = event.body.JSON;
+    const link = body.link
+    
+    
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ message: `Your link: '${link}'` })
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
